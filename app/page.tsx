8f2876137dcfec00b547a5c89064060ece2cc2f5
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2025-02-07 12:57:45 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -14,12 +14,12 @@ export default function Home() {
           className='object-cover -z-20'
         />
       </div>
-      <div className='flex items-center justify-between gap-10 z-10 max-w-7xl 2xl:max-w-[1440px]'>
-        <div className='relative w-[500px] aspect-[3000/2381]'>
+      <div className='flex items-center justify-between gap-14 z-10 max-w-7xl 2xl:max-w-[1440px]'>
+        <div className='relative w-[380px] aspect-[3000/2381]'>
           <Image src='/pmohive-logo.png' alt='PMO Hive logo' fill priority />
         </div>
         <div className=''>
-          <h1 className='text-6xl leading-snug font-medium text-black'>
+          <h1 className='text-6xl leading-tight font-medium text-black'>
             Our Website is
             <br />
             Coming Soon
@@ -29,7 +29,7 @@ export default function Home() {
           </p>
           <a
             href='mailto:clarke.shepherd@pmohive.com'
-            className='w-fit text-2xl text-gray-600 block border-b-2 border-accent transition-colors duration-200 delay ease-out hover:text-white relative before:content-[""] before:absolute before:inset-0 before:bg-accent before:w-0 before:transition-width before:duration-300 before:ease-out hover:before:w-full before:-z-10'
+            className='mt-2 w-fit text-2xl text-gray-600 block border-b-2 border-accent transition-colors duration-200 delay ease-out hover:text-white relative before:content-[""] before:absolute before:inset-0 before:bg-accent before:w-0 before:transition-width before:duration-300 before:ease-out hover:before:w-full before:-z-10'
           >
             clarke.shepherd@pmohive.com
           </a>
