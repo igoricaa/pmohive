c9f7d559b8f93d6a8e0869a8d4872c9346291c06
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2025-10-21 20:31:43 +0200

    about page intro section tweak

--- src/components/sections/about/intro-section.tsx
+++ src/components/sections/about/intro-section.tsx
@@ -1,4 +1,5 @@
 import Heading from '@/components/ui/heading';
+import { cn } from '@/lib/utils';
 import { urlForUncropped } from '@/sanity/lib/image';
 import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 import { Image } from 'next-sanity/image';
@@ -10,38 +11,47 @@ interface IntroSectionProps {
   };
   heading: string;
   backgroundImage: SanityImageSource & { alt: string };
+  className?: string;
 }
 
 const IntroSection = ({
   subtitle,
   heading,
   backgroundImage,
+  className,
 }: IntroSectionProps) => {
   return (
-    <section className='px-side relative sm:h-80 md:h-96 lg:h-120 xl:h-130 2xl:h-175 sm:flex sm:flex-col sm:justify-center'>
-      <div className='sm:w-6/8 xl:w-9/12 h-64 w-full sm:h-full sm:aspect-[580/302] xl:aspect-[991/478] relative sm:absolute sm:right-[var(--padding-side)] sm:top-0 -z-1 '>
-        <div className='absolute inset-0 bg-gradient-to-bl from-transparent from-30% to-black/60'></div>
-        <Image
-          src={urlForUncropped(backgroundImage).url()}
-          alt={backgroundImage.alt}
-          width={991}
-          height={478}
-          quality={100}
-          priority
-          className='object-cover w-full h-full'
-        />
-      </div>
+    <section
+      className={cn(
+        'px-side sm:h-80 md:h-96 lg:h-120 xl:h-130 2xl:h-175 sm:flex sm:flex-col sm:justify-center 2xl:grid 2xl:grid-cols-12',
+        className
+      )}
+    >
+      <div className='2xl:col-span-10 2xl:col-start-2 relative'>
+        <div className='sm:w-6/8 xl:w-9/12 h-64 w-full sm:h-full sm:aspect-[580/302] xl:aspect-[991/478] relative sm:absolute sm:right-0 sm:top-0 -z-1 '>
+          <div className='absolute inset-0 bg-gradient-to-bl from-transparent from-30% to-black/60'></div>
+          <Image
+            src={urlForUncropped(backgroundImage).url()}
+            alt={backgroundImage.alt}
+            width={991}
+            height={478}
+            quality={100}
+            priority
+            className='object-cover w-full h-full'
+          />
+        </div>
 
-      <Heading
-        level='h1'
-        subtitle={{
-          text: subtitle.text,
-          highlightedText: subtitle.highlightedText,
-        }}
-        className='sm:max-w-5/8 xl:max-w-5/12 -mt-8'
-      >
-        {heading}
-      </Heading>
+        <Heading
+          level='h1'
+          subtitle={{
+            text: subtitle.text,
+            highlightedText: subtitle.highlightedText,
+          }}
+          className='sm:max-w-5/8 xl:max-w-5/12 -mt-8'
+        >
+          {heading}
+        </Heading>
+      </div>
     </section>
   );
 };
